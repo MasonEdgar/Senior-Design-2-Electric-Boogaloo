1ce40e512e5cf5f47ec2746a19835a9de870fd1f
Nick-Archi <nicholas.archibong@gmail.com>
2017-10-11 19:54:15 -0500

    Creation of responseVector

--- Algorithm/AlgorithmScript.py
+++ Algorithm/AlgorithmScript.py
@@ -1,20 +1,30 @@
+import pdb
+
 print('Reading in of Data...')
 
 # read in data
 import csv
 import numpy as np
+from random import randint
 
 symptoms = []
-responseVector = []
 illnessMatrix = [] # matrix for illnesses and symptoms
 illnessList = [] # list of illnesses
 dataMatrix = [] # logical matrix
+responseVector = []
 
+print('Converting response vector -> numpy vector')    
 
 with open('SymptomList.csv', 'r') as file:
     read = csv.reader(file)
     for line in read:
         symptoms.append(line) # convert to list
+
+# read in symptoms into vector
+
+# creation of test responseVector
+for number in range(len(symptoms)):
+    responseVector.append(randint(0,1))
 
 with open('IllnessesMatrix.csv','r') as file:
     read = csv.reader(file)
