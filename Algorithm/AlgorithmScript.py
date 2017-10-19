1d0ccef5d63fb08d4f3745a0d8a1cd73268585bc
Nick-Archi <nicholas.archibong@gmail.com>
2017-10-18 23:39:19 -0500

    Changed some files and added some functionality to calculated the number of matches

--- Algorithm/AlgorithmScript.py
+++ Algorithm/AlgorithmScript.py
@@ -1,42 +1,55 @@
 import pdb
 
-print('Reading in of Data...')
-print('random stuff...')
-
 # read in data
 import csv
 import numpy as np
+import operator
 from random import randint
 
-symptoms = []
-illnessMatrix = [] # matrix for illnesses and symptoms
-illnessList = [] # list of illnesses
-dataMatrix = [] # logical matrix
-responseVector = []
+symptoms = [] # list that contains the symptoms
+illnessMatrix = [] # matrix for symptoms and illnesses
+dataMatrix = [] # logical matrix for symptoms and illnesses
+responseVector = [] # response vector from the GUI...as well as testing
+illnessDict = {} # dictionary for totalling of matches for each illness
+differentialDiag = [] # end differential diagnosis
 
-print('Converting response vector -> numpy vector')
-
+# read in symptoms into vector
 with open('SymptomList.csv', 'r') as file:
     read = csv.reader(file)
     for line in read:
-        symptoms.append(line) # convert to list
-
-# read in symptoms into vector
+        symptoms.append(line)
 
 # creation of test responseVector
 for number in range(len(symptoms)):
     responseVector.append(randint(0,1))
 
+# conversion to numpy array/vector
+responseVector = np.asarray(responseVector).astype('int')
+
 with open('IllnessesMatrix.csv','r') as file:
     read = csv.reader(file)
-    count = 0
     for line in read:
         illnessMatrix.append(line)
-    for illness in illnessMatrix:
-        count = count + 1
 
+# parse the illness and the symptom into separate variables
+illnessList = [item[0] for item in illnessMatrix] # list for illnesses
+symptomMatrix = [item[1:] for item in illnessMatrix]
 
-    #dataMatrix = np.asarray(dataMatrix)
+symptomMatrix = np.asarray(symptomMatrix).astype('int')
 
-#print(illnessMatrix)
-#print(illnessMatrix[1][1:69])
+# transpose responseVector for matrix multiplication...
+responseVector = np.transpose(responseVector)
+
+# matrix multiplication...
+for illness in range(len(illnessList)):
+    illnessDict[illnessList[illness]] = symptomMatrix[illness].dot(responseVector)
+
+#print(responseVector)
+#print(symptomMatrix)
+print(illnessDict)
+
+# for number in range(3):
+#     maxIllness = max(illnessDict.iteritems(), key = operator.itemgetter(1))[0]
+#     differentialDiag.append(maxIllness)
+#     illnessDict.pop(maxIllness, None)
+
