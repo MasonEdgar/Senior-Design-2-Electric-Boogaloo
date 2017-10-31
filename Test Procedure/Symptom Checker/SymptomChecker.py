bd2a3aac4fb99045f5592cfeaed01568e21299a6
MasonEdgar <mason.w.edgar@gmail.com>
2017-10-31 13:41:44 -0500

    Imported Illnesses
    .

--- Test Procedure/Symptom Checker/SymptomChecker.py
+++ Test Procedure/Symptom Checker/SymptomChecker.py
@@ -1,10 +1,28 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 31 12:29:42 2017
+'''
 
-@author: Nosam
-"""
+Author: Mason Edgar
+Symptom Checking Algorithm for the PMDK
+University of Houston 
+Senior Captson 2017
 
-print("\n\n\nHello world!")
+
+'''
+
+import numpy as np
+import pandas as pd
+df = pd.read_excel('/Users/Mason/Documents/Senior-Design-2-Electric-Boogaloo/Test Procedure/Symptom Checker/Illness Matrix Barebones .xlsx')
+illness_matrix = df.as_matrix()
+
+np.set_printoptions(threshold=np.nan)
+
+Anemia = illness_matrix[0]
+Bronchitis = illness_matrix[1]
+ColdSore = illness_matrix[2]
+Conjunctivitis = illness_matrix[3]
+Diabetes = illness_matrix[4]
+Chickenpox = illness_matrix[5]
+KidneyStones = illness_matrix[6]
+Migraines = illness_matrix[7]
+PollenAllergy = illness_matrix[8]
+Tuberculosis = illness_matrix[9]
 
