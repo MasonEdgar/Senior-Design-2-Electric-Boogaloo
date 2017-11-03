e9e1e628ec0390c666ce93e9de459621a72b38f5
MasonEdgar <mason.w.edgar@gmail.com>
2017-11-02 19:13:16 -0500

    Rest of Symptoms
    Should be all good and ready for the algo1 function

--- Test Procedure/Symptom Checker/SymptomChecker.py
+++ Test Procedure/Symptom Checker/SymptomChecker.py
@@ -186,7 +186,7 @@ stage = 1
 count = 0
 
 
-while(stage <= AnemiaSymptoms.shape[0]-9):
+while(stage <= AnemiaSymptoms.shape[0]):
     
     count = 0
     
@@ -217,25 +217,373 @@ while(stage <= AnemiaSymptoms.shape[0]-9):
 
 
 
+#--------------------- Bronchitis Block --------------------------#
+
+
+NewBronchitis = np.copy(Bronchitis)
+
+stage = 1
+count = 0
+
+
+while(stage <= BronchitisSymptoms.shape[0]):
+    
+    count = 0
+    
+    NewList = list(iter.combinations(BronchitisSymptoms, stage)) 
+    
+    for item in range(len(NewList)):
+        i = 0    
+        while(i < stage):
+            NewBronchitis[NewList[count][i]] = 0
+            i = i + 1
+            
+        #------ Algorithm Goes Here ------#    
+        
+        print(NewBronchitis)
+        print("")
+        
+        #---------------------------------#
+        
+        i = 0    
+        while(i < stage):
+            NewBronchitis[NewList[count][i]] = 1
+            i = i + 1
+        count = count + 1       
+        
+    stage = stage + 1
+    
+#---------------------------------------------------------------#
+
+
+
+
+#--------------------- Cold Sore Block --------------------------#
+
+
+NewColdSore = np.copy(ColdSore)
+
+stage = 1
+count = 0
+
+
+while(stage <= ColdSoreSymptoms.shape[0]):
+    
+    count = 0
+    
+    NewList = list(iter.combinations(ColdSoreSymptoms, stage)) 
+    
+    for item in range(len(NewList)):
+        i = 0    
+        while(i < stage):
+            NewColdSore[NewList[count][i]] = 0
+            i = i + 1
+            
+        #------ Algorithm Goes Here ------#    
+        
+        print(NewColdSore)
+        print("")
+        
+        #---------------------------------#
+        
+        i = 0    
+        while(i < stage):
+            NewColdSore[NewList[count][i]] = 1
+            i = i + 1
+        count = count + 1       
+        
+    stage = stage + 1
+    
+#---------------------------------------------------------------#
+
+
+
+
+#--------------------- Conjunctivitis Block --------------------------#
+
+
+NewConjunctivitis = np.copy(Conjunctivitis)
+
+stage = 1
+count = 0
+
+
+while(stage <= ConjunctivitisSymptoms.shape[0]):
+    
+    count = 0
+    
+    NewList = list(iter.combinations(ConjunctivitisSymptoms, stage)) 
+    
+    for item in range(len(NewList)):
+        i = 0    
+        while(i < stage):
+            NewConjunctivitis[NewList[count][i]] = 0
+            i = i + 1
+            
+        #------ Algorithm Goes Here ------#    
+        
+        print(NewConjunctivitis)
+        print("")
+        
+        #---------------------------------#
+        
+        i = 0    
+        while(i < stage):
+            NewConjunctivitis[NewList[count][i]] = 1
+            i = i + 1
+        count = count + 1       
+        
+    stage = stage + 1
+    
+#---------------------------------------------------------------------#
 
 
 
 
 
+#------------------------- Diabetes Block ----------------------------#
+
+
+NewDiabetes = np.copy(Diabetes)
+
+stage = 1
+count = 0
+
+
+while(stage <= DiabetesSymptoms.shape[0]):
+    
+    count = 0
+    
+    NewList = list(iter.combinations(DiabetesSymptoms, stage)) 
+    
+    for item in range(len(NewList)):
+        i = 0    
+        while(i < stage):
+            NewDiabetes[NewList[count][i]] = 0
+            i = i + 1
+            
+        #------ Algorithm Goes Here ------#    
+        
+        print(NewDiabetes)
+        print("")
+        
+        #---------------------------------#
+        
+        i = 0    
+        while(i < stage):
+            NewDiabetes[NewList[count][i]] = 1
+            i = i + 1
+        count = count + 1       
+        
+    stage = stage + 1
+    
+#---------------------------------------------------------------------#
 
 
 
 
 
+#------------------------- Chickenpox Block ----------------------------#
+
+
+NewChickenpox = np.copy(Chickenpox)
+
+stage = 1
+count = 0
+
+
+while(stage <= ChickenpoxSymptoms.shape[0]):
+    
+    count = 0
+    
+    NewList = list(iter.combinations(ChickenpoxSymptoms, stage)) 
+    
+    for item in range(len(NewList)):
+        i = 0    
+        while(i < stage):
+            NewChickenpox[NewList[count][i]] = 0
+            i = i + 1
+            
+        #------ Algorithm Goes Here ------#    
+        
+        print(NewChickenpox)
+        print("")
+        
+        #---------------------------------#
+        
+        i = 0    
+        while(i < stage):
+            NewChickenpox[NewList[count][i]] = 1
+            i = i + 1
+        count = count + 1       
+        
+    stage = stage + 1
+    
+#----------------------------------------------------------------------#
 
 
 
 
 
+#------------------------- Kidney Stones Block ----------------------------#
+
+
+NewKidneyStones = np.copy(KidneyStones)
+
+stage = 1
+count = 0
+
+
+while(stage <= KidneyStonesSymptoms.shape[0]):
+    
+    count = 0
+    
+    NewList = list(iter.combinations(KidneyStonesSymptoms, stage)) 
+    
+    for item in range(len(NewList)):
+        i = 0    
+        while(i < stage):
+            NewKidneyStones[NewList[count][i]] = 0
+            i = i + 1
+            
+        #------ Algorithm Goes Here ------#    
+        
+        print(NewKidneyStones)
+        print("")
+        
+        #---------------------------------#
+        
+        i = 0    
+        while(i < stage):
+            NewKidneyStones[NewList[count][i]] = 1
+            i = i + 1
+        count = count + 1       
+        
+    stage = stage + 1
+    
+#------------------------------------------------------------------------#
 
 
 
 
 
+#------------------------- Migraines Block ----------------------------#
 
+
+NewMigraines = np.copy(Migraines)
+
+stage = 1
+count = 0
+
+
+while(stage <= MigrainesSymptoms.shape[0]):
+    
+    count = 0
+    
+    NewList = list(iter.combinations(MigrainesSymptoms, stage)) 
+    
+    for item in range(len(NewList)):
+        i = 0    
+        while(i < stage):
+            NewMigraines[NewList[count][i]] = 0
+            i = i + 1
+            
+        #------ Algorithm Goes Here ------#    
+        
+        print(NewMigraines)
+        print("")
+        
+        #---------------------------------#
+        
+        i = 0    
+        while(i < stage):
+            NewMigraines[NewList[count][i]] = 1
+            i = i + 1
+        count = count + 1       
+        
+    stage = stage + 1
+    
+#---------------------------------------------------------------------#
+
+
+
+
+#------------------------ Pollen Allergy Block -----------------------#
+
+
+NewPollenAllergy = np.copy(PollenAllergy)
+
+stage = 1
+count = 0
+
+
+while(stage <= PollenAllergySymptoms.shape[0]):
+    
+    count = 0
+    
+    NewList = list(iter.combinations(PollenAllergySymptoms, stage)) 
+    
+    for item in range(len(NewList)):
+        i = 0    
+        while(i < stage):
+            NewPollenAllergy[NewList[count][i]] = 0
+            i = i + 1
+            
+        #------ Algorithm Goes Here ------#    
+        
+        print(NewPollenAllergy)
+        print("")
+        
+        #---------------------------------#
+        
+        i = 0    
+        while(i < stage):
+            NewPollenAllergy[NewList[count][i]] = 1
+            i = i + 1
+        count = count + 1       
+        
+    stage = stage + 1
+    
+#---------------------------------------------------------------------#
+
+
+
+
+#------------------------ Tuberculosis Block -------------------------#
+
+
+NewTuberculosis = np.copy(Tuberculosis)
+
+stage = 1
+count = 0
+
+
+while(stage <= TuberculosisSymptoms.shape[0]):
+    
+    count = 0
+    
+    NewList = list(iter.combinations(TuberculosisSymptoms, stage)) 
+    
+    for item in range(len(NewList)):
+        i = 0    
+        while(i < stage):
+            NewTuberculosis[NewList[count][i]] = 0
+            i = i + 1
+            
+        #------ Algorithm Goes Here ------#    
+        
+        print(NewTuberculosis)
+        print("")
+        
+        #---------------------------------#
+        
+        i = 0    
+        while(i < stage):
+            NewTuberculosis[NewList[count][i]] = 1
+            i = i + 1
+        count = count + 1       
+        
+    stage = stage + 1
+    
+#---------------------------------------------------------------------#
 
